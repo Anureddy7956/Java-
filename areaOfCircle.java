089a1a2d9a9607a9eb70810104b375c3c01659db
Anusha S Reddy <4q4947@gmail.com>
2026-08-22 09:12:50 +0530

    area of circle using final

---
+++ areaOfCircle.java
@@ -0,0 +1,9 @@
+public class areaOfCircle {
+    public static void main(String[] args) {
+       final double pi = 3.14;
+       int radius=4;
+       double area=pi*radius*radius;
+       System.out.println("The area of the circle is :"+ area);
+    }
+}
+
