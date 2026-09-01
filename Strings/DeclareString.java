4f58af06dcfa28378cb456f86a899e3744c28082
Anusha S Reddy <4q4947@gmail.com>
2026-09-01 20:35:39 +0530

    how to declare the string in java

---
+++ Strings/DeclareString.java
@@ -0,0 +1,9 @@
+package strings;
+public class DeclareString {
+    public static void main(String[] args) {
+        // declaring an string in two diffrent ways 
+        String str ="anya";
+        String str1 = new String("forger");
+        System.out.println("The daughter of loid forger is " + str + str1 );
+    }
+}
