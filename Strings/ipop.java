186a968909c24cec7c623c0c3b4a10dcc859b32f
Anusha S Reddy <4q4947@gmail.com>
2026-09-01 20:49:49 +0530

    input and output of the string

---
+++ Strings/ipop.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ipop {
+// input and output of a string
+   public static void main(String[] var0) {
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter the string : ");
+      String name = sc.next();
+      System.out.println(name);
+   }
+}
+
