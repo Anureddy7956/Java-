2443e0a09ebbda44d66472c7fdefff5b25f601a9
Anusha S Reddy <4q4947@gmail.com>
2026-09-01 21:01:41 +0530

    length of the string

---
+++ Strings/StrLen.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class StrLen {
+   public static void main(String[] var0) {
+   // to print the string length
+   Scanner sc= new Scanner(System.in);
+   String name = sc.nextLine();
+   System.out.println(name.length());
+
+   }
+}
+
