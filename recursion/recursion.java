2fd0de27e250ef167deb442c5abbecc7d10f1a8b
Anusha S Reddy <4q4947@gmail.com>
2026-09-02 10:02:55 +0530

    factorial using recursion

--- recursion/recursion.java
+++ recursion/recursion.java
@@ -3,13 +3,13 @@ public class recursion {
     static int sum(int n) {
         // Base condition
         if (n < 1) 
-            return 0;
-        return n+sum(n-1);    
+            return 1;
+        return n*sum(n-1);    
     }
    
 
     public static void main(String[] args) {
-        int res=sum(10);
+        int res=sum(5);
         System.out.println(res);
     }
 }
