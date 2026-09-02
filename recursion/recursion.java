973b35e6f68048cc744d4f25495edc0c023ef94a
Anusha S Reddy <4q4947@gmail.com>
2026-09-02 10:00:42 +0530

    sum of the numbers

--- recursion/recursion.java
+++ recursion/recursion.java
@@ -1,13 +1,15 @@
 public class recursion {
-    static void say_hello(int n){
-        // Recursion from top to bottom 
-        if(n<1){ //base condition
-        return;
-        }
-        System.out.println(" hello");
-        say_hello(n-1);
+
+    static int sum(int n) {
+        // Base condition
+        if (n < 1) 
+            return 0;
+        return n+sum(n-1);    
     }
+   
+
     public static void main(String[] args) {
-        say_hello(5);
+        int res=sum(10);
+        System.out.println(res);
     }
 }
